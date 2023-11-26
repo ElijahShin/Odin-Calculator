178d311fc90f6451294c41315c337c6bba757949
Elijah Shin <44964271+ElijahShin@users.noreply.github.com>
2023-11-26 11:14:07 -0800

    Add operate function

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+let val1 = 0;
+let val2 = 0;
+let operator = '';
+
 function add(val1, val2) {
   return val1 + val2;
 }
@@ -12,4 +16,21 @@ function multiply(val1, val2) {
 
 function divide(val1, val2) {
   return val1 / val2;
+}
+
+function operate(val1, val2, operator) {
+  switch(operator) {
+    case '+':
+      console.log(add(val1, val2));
+    break;
+    case '-':
+      console.log(subtract(val1, val2));
+    break;
+    case '*':
+      console.log(multiply(val1, val2));
+    break;
+    case '/':
+      console.log(divide(val1, val2));
+    break;
+  }
 }
